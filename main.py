3bfea8df765e3f601aa3dd6ff3b1d30b6c8b54c2
qwerty <wolf20102014@mail.ru>
2021-10-03 18:22:31 +0300

    add register page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 
 app = Flask(__name__)
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def index():
 
     return "hello world"
+
+
+@app.route("/login",methods=["POST","GET"])
+def login():
+
+    return render_template("login.htm")
 
 
 if __name__ == "__main__":
